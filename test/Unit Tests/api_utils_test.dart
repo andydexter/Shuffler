2879c15d08a9b1212dcdd4c62e6e41a50599e02f
andydexter <andydexter123@gmail.com>
2024-08-28 22:30:09 +0300

    fixed static analysis comment issues

--- test/Unit Tests/api_utils_test.dart
+++ test/Unit Tests/api_utils_test.dart
@@ -1,24 +1,25 @@
-/***
+///
+///     Copyright (C) 2024  Andreas Nicolaou
+///
+///     This program is free software: you can redistribute it and/or modify
+///     it under the terms of the GNU General Public License as published by
+///     the Free Software Foundation, either version 3 of the License, or
+///     (at your option) any later version.
+///
+///     This program is distributed in the hope that it will be useful,
+///     but WITHOUT ANY WARRANTY; without even the implied warranty of
+///     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+///     GNU General Public License for more details.
+///
+///     You should have received a copy of the GNU General Public License
+///     along with this program. You can find it at project root.
+///     If not, see <https://www.gnu.org/licenses/>.
+///
+///     Author E-mail address: andydexter123@gmail.com
+///
 
-    Copyright (C) 2024  Andreas Nicolaou
+library;
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program. You can find it at project root.
-    If not, see <https://www.gnu.org/licenses/>.
-
-    Author E-mail address: andydexter123@gmail.com
-
-***/
 import 'dart:convert';
 import 'dart:io';
 
